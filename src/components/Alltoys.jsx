6e25aa263f6340cd9a91842deed37d9b77bb6370
unknown <nakib15-3358@diu.edu.bd>
2023-05-23 04:52:26 +0600

    search operation added

--- src/components/Alltoys.jsx
+++ src/components/Alltoys.jsx
@@ -9,7 +9,6 @@ const Alltoys = () => {
   const backendUrl = import.meta.env.VITE_backendUrl;
   // const backendUrl = "http://localhost:3000";
 
-
   useEffect(() => {
     const fetchToy = async () => {
       setLoading(true);
@@ -21,20 +20,44 @@ const Alltoys = () => {
       } catch (error) {
         console.error("Error fetching data:", error);
       }
-    }
+    };
     fetchToy();
   }, []);
 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = () => {
+    const filtered = [...toys].filter((toy) =>
+      toy.toyName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setToys(filtered);
+    setSearchTerm("")
+  };
+
   return (
     <div className="my-16 space-y-8">
-      <h1 className="text-center text-2xl">All Toys</h1>
-      {
-        loading && (
-          <div className="min-h-screen flex justify-center items-center">
-            <h1 className="text-4xl font-semibold text-center">Loading....</h1>
-          </div>
-        )
-      }
+      <h1 className="text-3xl lg:text-5xl font-bold text-center text-gray-600">
+        All Toys
+      </h1>
+      <div className="flex justify-center items-center gap-6 mx-6">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          className="w-[75%] px-5 py-3 border-2 border-solid border-gray-300 rounded-2xl"
+        />
+        <button className="btn btn-secondary" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+      {loading && (
+        <div className="min-h-screen flex justify-center items-center">
+          <h1 className="text-4xl font-semibold text-center">Loading....</h1>
+        </div>
+      )}
       <div className="mx-6 xl:max-w-[1200px] xl:mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
         {toys?.map((toy) => (
           <Toy key={toy._id} toy={toy} />
