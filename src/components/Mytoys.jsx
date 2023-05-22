9b9e2c9b8ae1977ba1723228970b5d6d998cb1f0
unknown <nakib15-3358@diu.edu.bd>
2023-05-22 23:33:17 +0600

    my toy page done

--- src/components/Mytoys.jsx
+++ src/components/Mytoys.jsx
@@ -4,21 +4,30 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../providers/AuthProviders";
 
 const Mytoys = () => {
+  const [loading, setLoading] = useState(false);
   const [userToys, setUserToys] = useState([]);
   const backendUrl = import.meta.env.VITE_backendUrl;
   const { user } = useContext(AuthContext);
   useEffect(() => {
+    setLoading(true);
     fetch(`${backendUrl}/userToys?email=${user?.email}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         setUserToys(data);
+        setLoading(false);
       });
   }, []);
+
   return (
     <div className="min-h-screen my-16">
+      {loading && (
+        <div className="min-h-screen flex justify-center items-center">
+          <h1 className="text-4xl font-semibold text-center">Loading....</h1>
+        </div>
+      )}
       <h1 className="text-2xl lg:text-4xl text-center font-bold">My Toys</h1>
-      <div className="mx-6 lg:mx-10 xl:mx-auto xl:max-w-[1200px]">
+      <div className="mx-6 lg:mx-10 xl:mx-auto xl:max-w-[900px]">
         {userToys.length < 1 ? (
           <>
             <div className="flex justify-center items-center flex-col min-h-screen space-y-6">
@@ -31,9 +40,11 @@ const Mytoys = () => {
         ) : (
           <>
             <div className="my-8 ">
-              {userToys?.map((toy) => {
-                <UserToy key={toy._id} toy={toy} />;
-              })}
+              <div className=" w-full flex flex-col justify-center gap-y-5">
+                {userToys?.map((toy) => {
+                  return <UserToy key={toy._id} toy={toy} />;
+                })}
+              </div>
             </div>
           </>
         )}
--- src/components/UserToy.jsx
+++ src/components/UserToy.jsx
@@ -1,9 +1,26 @@
-
-const UserToy = ({toy}) => {
-    console.log(toy);
+import {FcRating} from "react-icons/fc"
+const UserToy = ({ toy }) => {
+  console.log(toy);
+  const { category, picture, price, rating, toyName } = toy;
   return (
-    <div>UserToy</div>
-  )
-}
+    <div className="w-full p-4 border-2 border-solid rounded-3xl flex justify-between items-center gap-2 lg:gap-4 xl:gap-5 shadow-lg lg:shadow-xl">
+      <div>
+        <img
+          src={picture || "https://picsum.photos/200/300"}
+          alt={toyName}
+          className="w-[60px] rounded-xl bg-gray-400"
+        />
+      </div>
+      <div className="flex flex-col gap-0">
+        <h2 className="text-base font-medium">{toyName}</h2>
+        <p>{category}</p>
+      </div>
+      <p className="py-2 text-base lg:text-lg font-semibold">{price + "  $"}</p>
+      <p className="py-2 text-base lg:text-lg font-semibold flex items-center gap-1">{rating + " "} <FcRating /></p>
 
-export default UserToy
+      <button className="btn btn-sm">Delete</button>
+    </div>
+  );
+};
+
+export default UserToy;
